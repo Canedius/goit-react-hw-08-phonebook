1b914c5071db2784e1470be484b7930ee29c51cb
Denys <kanedius@gmail.com>
2022-07-01 14:47:32 +0300

    add

--- src/components/Views/LoginView.jsx
+++ src/components/Views/LoginView.jsx
@@ -1,5 +1,12 @@
 import LoginForm from "components/LoginForm/LoginForm"
+import { useSelector } from "react-redux";
+import { getInitialization } from "redux/contacts/contact-selectors";
+import { Navigate } from "react-router-dom";
 function LoginView(params) {
+    const initialization = useSelector(getInitialization)
+    if (initialization) {
+        return <Navigate to ="/contacts"/>
+    }
     return <LoginForm/>
 }
 export default LoginView
--- src/components/Views/RegisterView.jsx
+++ src/components/Views/RegisterView.jsx
@@ -1,5 +1,12 @@
 import { RegForm } from "components/RegForm/RegForm"
+import { useSelector } from "react-redux";
+import { getInitialization } from "redux/contacts/contact-selectors";
+import { Navigate } from "react-router-dom";
 function RegisterView(params) {
+    const initialization = useSelector(getInitialization)
+    if (initialization) {
+        return <Navigate to ="/contacts"/>
+    }  
     return <RegForm/>
 }
 
